b36daa413b18ae7577ca6e6a61ce5ec34c37f731
wztan004 <tanwzwz@gmail.com>
2023-03-18 18:39:41 +0800

    t

---
+++ decorator/decorator.py
@@ -0,0 +1,14 @@
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_whee():
+    print("Whee!")
+
+
+if __name__ == "__main__":
+    say_whee()
